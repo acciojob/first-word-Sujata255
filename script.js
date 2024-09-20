bcc02f07fe60b062223cc3a535e58a8bd9f8daa1
Sujata255 <das19sujata@gmail.com>
2024-09-20 19:22:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  if (str.length === 0 || !str.includes(" ")) {
+    return str;
+  }
+
+  const firstSpaceIndex = str.indexOf(" ");
+  return str.substring(0, firstSpaceIndex);
 }
 
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
+console.log(firstWord("see and stop")); // Output: see
+console.log(firstWord("Hello World!")); // Output: Hello
+console.log(firstWord("12345")); // Output: 12345
+console.log(firstWord("")); // Output:   
